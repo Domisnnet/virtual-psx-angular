bbab469eb8fb8707b287986baaafb0d468e57746
dominique.goncalves61 <dominique.goncalves61@gmail.com>
2026-06-22 18:39:45 +0000

    adicionados

---
+++ src/app/components/card/card-label/card-label.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-label-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-label.component.html',
+  styleUrls: ['./card-label.component.scss'],
+})
+export class CardLabelComponent {}
---
+++ src/app/components/card/card-pricing/card-pricing.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-pricing-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-pricing.component.html',
+  styleUrls: ['./card-pricing.component.scss'],
+})
+export class CardPricingComponent {}
